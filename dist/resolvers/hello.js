cc6fa20bfd7c9f7ba9ce81aa3d530c381eb52762
xh-lin <xuhuanglin99@gmail.com>
2023-10-11 16:29:36 -0400

    nodejs gitignore

--- dist/resolvers/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolvers = {
-    Query: {
-        hello: () => "Hello world!"
-    }
-};
-exports.default = resolvers;
-//# sourceMappingURL=hello.js.map
